45370cc1aa160cbbbf32a4db11604b84a821d244
Jamparapu Tharun <160770548+Toftharun999@users.noreply.github.com>
2026-01-20 17:10:42 +0530

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -6,7 +6,7 @@ const person: Person = {
   lastName: "Tharun",
   name: `Jamparapu Tharun`,
   role: "Digital Marketing Executive and Web Developer ",
-  avatar:,
+  avatar: "/images/avatar.jpg",
   email: "tharunjamparapu@gmail.com",
   location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English","Telugu","Hindi" ], // optional: Leave the array empty if you don't want to display languages
